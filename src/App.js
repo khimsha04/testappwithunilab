b1f909fb9c143a2ab77027d174c2c9de37df4fd4
Zurab Khimshiashvili <64226929+khimsha04@users.noreply.github.com>
2023-04-28 10:19:30 +0400

    Finish first simple page none react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Logo from './Images/Logo.png';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <div className="App">
+        <img className='logoTodo' src={Logo} alt={'imagasde'}/>
+        <h1 className='firstPageH1'>Keep Track Of Daily Tasks In Life</h1>
+        <button className='firstPageButton'>Get Started</button>
+      </div>
   );
 }
 
